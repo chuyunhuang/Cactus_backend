3ef22d4d874ecadb531df83ab8352ff20e9d0126
Chuyun Huang <chuyunhuang@Chuyuns-MacBook-Air.local>
2019-03-18 16:22:48 -0400

    heroku changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {app,} = require('./app')
 
-app.listen(3200,() =>{
-    console.log('Server listening on port 3200')
+app.listen(process.env.PORT || 3100,() =>{
+    console.log(`Server listening on port ${process.env.PORT || 3100}`)
 })
