441c62c41795f5127dcc8f680785d9c654664bb6
Chuyun Huang <chuyunhuang@pursuit.org>
2019-03-20 16:53:00 -0400

    completed user router and services

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,12 @@ userRouter.post('/', (req, res)=>{
     userService.create(req.body)
     .then(()=>{
         res.status(200)
-        res.json({"success": true})
+        res.json({"successfully created user": true})
     })
     .catch(err=>{
         res.status(400)
         res.json({
-            "success": false,
+            "successfully created user": false,
             "err": err,
         })
     })
@@ -38,6 +38,43 @@ userRouter.get('/', (req, res)=>{
     })
 })
 
+//Update User
+userRouter.put('/:id', (req, res)=>{
+    const {id} = req.params;
+    const {username, email, avatar} =req.body;
+
+    userService.update(id, username, email, avatar)
+    .then(()=>{
+        res.status(200)
+        res.json({"successfully updated user":true})
+    })
+    .catch(err=>{
+        res.status(400)
+        res.json({
+            "successfully updated user":false,
+            "err": err,
+        })
+    })
+})
+
+
+userRouter.delete('/:id',(req, res)=>{
+    const {id} = req.params;
+    userService.delete(id)
+    .then(()=>{
+        res.status(200)
+        res.json({
+            "successfully deleted user": true
+        })
+    })
+    .catch(err=>{
+        res.status(400)
+        res.json({
+            "successfully deleted user": false,
+            "err": err
+        })
+    })
+})
 
 
 module.exports = userRouter;
--- services/userService.js
+++ services/userService.js
@@ -2,12 +2,21 @@ const db = require('./dbConnect');
 const userService = {}
 
 userService.create = (body)=>{
-    const {name, email, phoneNum} = body;
-    return db.none('INSERT INTO users (name, email, phoneNum) VALUES ([name], [email], [phoneNum])', {name, email, phoneNum})
+    const {username, email, avatar} = body;
+    return db.none('INSERT INTO users (username, email, avatar) VALUES (${username}, ${email}, ${avatar})', {username, email, avatar})
 }
 
 userService.read = () =>{
     return db.any('SELECT * FROM users')
 }
+
+userService.update = (id, username, email, avatar) =>{
+    return db.none('UPDATE users SET username = ${username}, email = ${email}, avatar = ${avatar} WHERE id = ${id}', {id, username, email, avatar})
+}
+
+userService.delete = (id) =>{
+    return db.none('DELETE FROM users WHERE id = ${id}', {id})
+}
+
 
 module.exports = userService;
