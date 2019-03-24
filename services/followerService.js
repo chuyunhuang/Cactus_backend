64836a82ccaa1f7c1e0f8f680f9983cbb5cb0ee5
Chuyun (YUN) Huang <chuyunhuang@pursuit.org>
2019-03-24 12:25:32 -0400

    implemented follower service and routes, and re-seeded the db (#8)

---
+++ services/followerService.js
@@ -0,0 +1,25 @@
+const db = require('./dbConnect')
+
+const followerService = {}
+
+followerService.create = (body) =>{
+    const {follower_id, following_id} =body
+    return db.none('INSERT INTO followers (follower_id, following_id) VALUES (${follower_id}, ${following_id})', {follower_id, following_id})
+}
+
+//get one's following
+followerService.read = (follower_id) =>{
+    return db.any('SELECT following_id FROM followers WHERE follower_id = ${follower_id}', {follower_id})
+}
+
+//get one's follower
+followerService.read2 = (following_id) =>{
+    return db.any('SELECT follower_id FROM followers WHERE following_id = ${following_id}', {following_id})
+
+}
+
+followerService.delete = (follower_id, following_id) =>{
+    return db.none('DELETE FROM followers WHERE follower_id = ${follower_id} AND following_id = ${following_id}', {follower_id, following_id})
+}
+
+module.exports = followerService;
