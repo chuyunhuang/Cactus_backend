b4e86c97615bbbe3438cf513fbfc8e598ee54db1
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-20 12:06:56 -0400

    added mysql pkg and fix dbConnect for Heroku

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,4 +1,17 @@
-const pgp = require('pg-promise')({});
-const db = pgp('postgres://localhost/cactus');
+const pgp = require("pg-promise")({});
+// const db = pgp('postgres://localhost/cactus');
+const mysql = require("mysql");
+const db = getConnection();
 
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "bd00d2f32ef759",
+  password: "aa462a62",
+  database: "heroku_a4d73e7dc681c99"
+});
+
+function getConnection() {
+  return pool;
+}
 module.exports = db;
