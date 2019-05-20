a2c598f40298d43139c00767b884b0cd05bb74ac
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-20 11:15:43 -0400

    changed routes and services

--- services/commentService.js
+++ services/commentService.js
@@ -1,24 +1,33 @@
-const db = require('./dbConnect');
+const db = require("./dbConnect");
 
-const commentService = {}
+const commentService = {};
 
-commentService.create = (post_id, author_id, comment_text) =>{
-    return db.none('INSERT INTO comments ( post_id, author_id, comment_text) VALUES (${post_id}, ${author_id}, ${comment_text})', {post_id, author_id, comment_text})
-}
+commentService.create = (post_id, author_id, comment_text) => {
+  return db.none(
+    "INSERT INTO comments ( post_id, author_id, comment_text) VALUES (${post_id}, ${author_id}, ${comment_text})",
+    { post_id, author_id, comment_text }
+  );
+};
 
-commentService.read = (post_id) =>{
-    return db.any(`SELECT * 
+commentService.read = post_id => {
+  return db.any(
+    `SELECT * 
 	FROM comments JOIN users 
 		ON comments.author_id = users.useruid 
-			WHERE post_id = $[post_id]`, {post_id}) 
-}
+			WHERE post_id = $[post_id]`,
+    { post_id }
+  );
+};
 
-commentService.update = (id, comment_text) =>{
-   return db.none('UPDATE comments SET comment_text = ${comment_text} WHERE id = ${id}', {id, comment_text})
-}
+commentService.update = (id, comment_text) => {
+  return db.none(
+    "UPDATE comments SET comment_text = ${comment_text} WHERE id = ${id}",
+    { id, comment_text }
+  );
+};
 
-commentService.delete = (id) =>{
-    return db.none('DELETE FROM comments WHERE id = ${id}', {id})
-}
+commentService.delete = id => {
+  return db.none("DELETE FROM comments WHERE id = ${id}", { id });
+};
 
 module.exports = commentService;
--- services/postService.js
+++ services/postService.js
@@ -1,26 +1,35 @@
-const db = require('./dbConnect');
+const db = require("./dbConnect");
 
-const postService = {}
+const postService = {};
 
-postService.create = (body) =>{
-    const {author_id, image_url, caption} = body
-    return db.none('INSERT INTO posts (author_id, image_url, caption) VALUES ( ${author_id}, ${image_url}, ${caption})', {author_id, image_url, caption})
-}
+postService.create = body => {
+  const { author_id, image_url, caption } = body;
+  return db.none(
+    "INSERT INTO posts (author_id, image_url, caption) VALUES ( ${author_id}, ${image_url}, ${caption})",
+    { author_id, image_url, caption }
+  );
+};
 
-postService.readNewsfeed = () =>{
-    return db.any('SELECT * FROM users INNER JOIN posts ON useruid = author_id')
-}
+postService.readNewsfeed = () => {
+  return db.any("SELECT * FROM users INNER JOIN posts ON useruid = author_id");
+};
 
-postService.readbyuid = (useruid) =>{
-    return db.any('SELECT image_url, caption FROM posts JOIN users ON useruid = author_id WHERE useruid = ${useruid}', {useruid})
-}
+postService.readbyuid = useruid => {
+  return db.any(
+    "SELECT image_url, caption FROM posts JOIN users ON useruid = author_id WHERE useruid = ${useruid}",
+    { useruid }
+  );
+};
 
-postService.update = (id, image_url, caption) =>{
-    return db.none('UPDATE posts SET image_url = ${image_url}, caption = ${caption} WHERE id = ${id}', {id, image_url, caption})
-}
+postService.update = (id, image_url, caption) => {
+  return db.none(
+    "UPDATE posts SET image_url = ${image_url}, caption = ${caption} WHERE id = ${id}",
+    { id, image_url, caption }
+  );
+};
 
-postService.delete = (id)=>{
-    return db.none('DELETE FROM posts WHERE id = ${id}', {id})
-}
+postService.delete = id => {
+  return db.none("DELETE FROM posts WHERE id = ${id}", { id });
+};
 
 module.exports = postService;
