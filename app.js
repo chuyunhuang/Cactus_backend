0ce73e64c7560596ea2328ace1b01f3973db9f04
Chuyun Huang <chuyunhuang@pursuit.org>
2019-05-20 11:27:57 -0400

    testing

--- app.js
+++ app.js
@@ -1,48 +1,51 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const admin = require('./firebase');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const admin = require("./firebase");
 
 const app = express();
-const userRouter = require('./routes/user');
-const postRouter = require('./routes/post');
-const commentRouter = require('./routes/comment');
-const likeRouter = require('./routes/like');
-const searchRouter = require('./routes/search');
-const followerRouter = require('./routes/follower');
+const userRouter = require("./routes/user");
+const postRouter = require("./routes/post");
+const commentRouter = require("./routes/comment");
+const likeRouter = require("./routes/like");
+const searchRouter = require("./routes/search");
+const followerRouter = require("./routes/follower");
 
 //Middleware
 app.use(cors());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 //Firebase-admin
-const checkFirebaseToken = (req, res, next) =>{
-    const token = req.body
+const checkFirebaseToken = (req, res, next) => {
+  const token = req.body;
 
-    admin.auth().verifyIdToken(token)
-    .then(function(decodedToken){
-        var uid = decodedToken.uid
-        next();
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(function(decodedToken) {
+      var uid = decodedToken.uid;
+      next();
     })
-    .catch(err =>{
-        res.json(err)
-    })
-}
+    .catch(err => {
+      res.json(err);
+    });
+};
 
-app.post('/test', checkFirebaseToken, (req, res)=>{
-    res.json({"express app working": true})
+app.post("/test", checkFirebaseToken, (req, res) => {
+  res.json({ "express app working": true });
 });
 
+app.get("/", (req, res) => {
+  res.send("HELLLLLLOOOOO!!!!");
+});
 
+app.use("/user", userRouter);
+app.use("/post", postRouter);
+app.use("/comment", commentRouter);
+app.use("/like", likeRouter);
+app.use("/search", searchRouter);
+app.use("/follower", followerRouter);
 
-
-app.use('/user', userRouter)
-app.use('/post', postRouter)
-app.use('/comment', commentRouter)
-app.use('/like', likeRouter)
-app.use('/search', searchRouter)
-app.use('/follower', followerRouter)
-
-module.exports = {app,}
+module.exports = { app };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {app,} = require('./app')
+const { app } = require("./app");
 
-app.listen(process.env.PORT || 3100,() =>{
-    console.log(`Server listening on port ${process.env.PORT || 3100}`)
-})
+app.listen(process.env.PORT || 3100, () => {
+  console.log(`Server listening on port ${process.env.PORT || 3100}`);
+});
